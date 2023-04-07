e64158dfae81abc87ec91a4c1d1aee9fab71fb15
JsonLu0910 <jasonlu0910@gmail.com>
2023-04-07 08:28:53 +0800

    Test for tailscale

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   apps : [{
+//     name   : "app1",
+//     script : "./src/App.js"
+//   }]
+// }
+
+module.exports = {
+  apps : [{
+    name   : "app1",
+    script : "./build/index.html"
+  }]
+}
